f3a670d855e728c2de6f3831eef477734f84f771
Stephen <st-jeeves@hotmail.co.uk>
2020-02-18 09:36:22 +0000

    ruby recap homework

---
+++ precouse_recap.rb
@@ -0,0 +1,36 @@
+#conditional
+
+weather = "sunny"
+if weather = "sunny"
+puts "I need my sunglasses"
+else puts "I need my jacket"
+end
+
+#function
+
+def greet name
+print "hello #{name}"
+end
+
+#array
+
+my_array [1, 2, 3, 4, 5]
+
+#hash
+
+my_hash = { "name" => "Stephen",
+  "age" => 29,
+  "hungry?" => true
+}
+
+#loop
+
+counter = 1
+while counter <= 10
+  p counter
+  counter += 1
+  if counter = 11
+  p counter
+end
+
+#
